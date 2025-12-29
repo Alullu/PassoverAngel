8c44b3f4e555e7e5c49056eb449368f051ffb6ca
Alullu <136066813+Alullu@users.noreply.github.com>
2025-12-29 11:51:02 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,9 +4,14 @@
 		$(".mobile-navigation").append($(".main-navigation .menu").clone());
 
 		// Mobile menu toggle 
-		$(".menu-toggle").click(function(){
+		$(".menu-toggle").on("click", function(){
 			$(".mobile-navigation").slideToggle();
 		});
+
+  		$("[class^=year]").each((i, el) => {
+  			var year = new Date().getFullYear();
+  			el.innerHTML = year + (parseInt(el.className.replace("year", "")) || 0);
+  		});
 
 	    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
